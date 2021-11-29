c0befe3bd5a5ad9ff4db5a9472653fd29580f20e
TrevorZylks <trevorzylks@gmail.com>
2021-11-29 13:40:03 -0700

    init

--- example-project/client/src/App.js
+++ example-project/client/src/App.js
@@ -11,18 +11,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <h1>Hi</h1>
   );
 }
 
